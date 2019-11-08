c67b13c3eeca828d3b05dd468e49e8f80fe478ca
Arhio <Arhio@yandex.ru>
2019-11-08 19:19:16 +0300

    Алгоритмы. Задание 7. Начальное оформление проекта.

--- GB-Algoritmen-Lesson_7/Program.cs
+++ GB-Algoritmen-Lesson_7/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibraryFastDecisions;
+using static System.Console;
+using System.Reflection;
 
 namespace GB_Algoritmen_Lesson_7
 {
     class Program
     {
+        static Dictionary<string, Act> dict = new Dictionary<string, Act>
+        {
+            { "1", new Act },
+        };
+
         static void Main(string[] args)
         {
+            var ex = new Extension();
+            var q = new Questions();
+            var n = "";
+            WriteLine("С# - Алгоритмы и структуры данных. Задание 7.");
+            WriteLine("Кузнецов");
+            while (n != "0")
+            {
+                WriteLine("Введите номер интересующей вас задачи:" + Environment.NewLine +
+                    "1.	Написать функции, которые считывают матрицу смежности из файла и выводят её на экран." + Environment.NewLine +
+                    "2. Написать рекурсивную функцию обхода графа в глубину." + Environment.NewLine +
+                    "3. Написать функцию обхода графа в ширину." + Environment.NewLine +
+                    "4.  * Создать библиотеку функций для работы с графами." + Environment.NewLine +
+                    "0. Нажмите для выхода из программы.");
+
+                n = q.Question<int>("Введите ", new HashSet<char>() { '0', '1', '2', '3', '4' }, true);
+                if (n == "0") break;
+                dict[n].Work();
+            }
+
+            Console.ReadKey();
         }
+    }
+
+    abstract class Act
+    {
+        public abstract void Work();
     }
 }
